8ff0b9fd586e307ce09b8c153a84ebf31f7d0fff
SESH1337 <51382974+SESH1337@users.noreply.github.com>
2020-09-05 15:32:38 +0400

    chnager
    revolution

--- vanilla.js
+++ vanilla.js
@@ -5,34 +5,70 @@ let dom = document;
 
 let searchHTML = dom.querySelector('.search');
 let searchJS = dom.querySelector('.search-input');
-let bodyy = dom.querySelectorAll('body');
+let input = dom.querySelector('.inputForJs');
+
+
+// function conditionBody(){
+//     if()
+// }
 
 
 
 
 
 
-searchHTML.addEventListener('click', function(e){
-    if(searchHTML.style.display = "none"){
-        searchJS.style.display = "block";
-    }else if(searchJS === searchHTML.style.bodyy){
-        searchHTML.style.display = "none"
-    }
-});
+
 
 
 let languages = dom.querySelector('.cont-lang');
-let outLang = dom.querySelector('.language')
+let outLang = dom.querySelector('.language');
 
-// languages.addEventListener('click', function(){
-//     if(languages.style.display = "none" || languages.style.className = "cont-lang"){
-
-//         languages.style.display = "block";
-//     }
-// }
-
-outLang.addEventListener('click', function(){
-    if(languages.style.className = "cont-lang"){
+outLang.addEventListener('click', function(event){
+    event.stopPropagation();
+    if(languages.style.display = "none"){
     languages.style.display = "block";
+    }else{
+        out.style.display = "none";
     }
 });
+
+
+
+
+let bodyElm = dom.body;
+
+
+
+    searchHTML.addEventListener('click', function(e){
+        e.stopPropagation();
+        if(searchJS.style.display = "none"){
+            searchJS.style.display = "block";
+        }else{
+            searchJS.style.display = "block";
+        }
+    });     
+
+
+    bodyElm.addEventListener('click', function(event){
+    
+        if(event.target !== searchJS){
+            searchJS.style.display = "none";
+    }else{
+        input.style.display = "block";
+    }
+ });
+
+ bodyElm.addEventListener('click', function(event){
+    
+    if(event.target !== languages){
+        languages.style.display = "none";
+}else{
+    input.style.display = "block";
+}
+});
+
+ 
+
+
+
+
