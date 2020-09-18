b7ab6aca3c82dd7bcf0024f96136c15701c7580f
SESH1337 <51382974+SESH1337@users.noreply.github.com>
2020-09-18 12:14:16 +0400

    newedit
    createas

--- vanilla.js
+++ vanilla.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+// alert('sdf')
 let dom = document;
 
 let searchHTML = dom.querySelector('.search');
@@ -21,16 +21,23 @@ let input = dom.querySelector('.inputForJs');
 
 
 let languages = dom.querySelector('.cont-lang');
-let outLang = dom.querySelector('.language');
+let outLang = dom.querySelector('.language > a');
 
 outLang.addEventListener('click', function(event){
-    event.stopPropagation();
-    if(languages.style.display = "none"){
-    languages.style.display = "block";
-    }else{
-        out.style.display = "none";
+    const display = languages.style.display;
+
+    if (display === 'block') {
+        languages.style.display = 'none';
+    } else {
+        languages.style.display = 'block';
     }
-});
+ });
+
+
+ outLang.addEventListener('blur', function(event){
+    // event.stopPropagation();
+    languages.style.display = "none";
+ });
 
 
 
@@ -58,17 +65,57 @@ let bodyElm = dom.body;
     }
  });
 
- bodyElm.addEventListener('click', function(event){
-    
-    if(event.target !== languages){
-        languages.style.display = "none";
-}else{
-    input.style.display = "block";
-}
-});
+//  bodyElm.addEventListener('click', function(event){
+//    if(event.target !== outLang){
+//         languages.style.display = "none";
+//     }
+
+//     console.log(event, languages);
+
+// });
 
  
 
 
 
 
+//                     ABOUT US ///////////////
+
+
+
+let tabsBtn = dom.querySelectorAll(".tabs__nav-btn");
+let tabsItems = dom.querySelectorAll(".tabs__item");
+let firstChild = dom.querySelector(".tabs__nav :first-child");
+
+// firstChild.addEventListener('click',function(){
+//     console.log("beeee")
+// })
+
+tabsBtn.forEach(function(item){
+    item.addEventListener('click', function(){
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+     
+        tabsBtn.forEach(function(item){
+            item.classList.remove('active');
+          });
+
+        tabsItems.forEach(function(item){
+            item.classList.remove('addText');
+          }); 
+
+
+
+            currentBtn.classList.add('active');
+            currentTab.classList.add('addText');
+    });
+
+    // if(item != firstChild){
+    //     // item.stopPropagation();
+    //     firstChild.style.color = "#2d3e50";
+    // }else{
+    //     item.classList.add('active');
+        
+    // }
+})
